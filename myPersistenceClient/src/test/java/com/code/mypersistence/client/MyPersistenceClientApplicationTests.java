049b75932c7beb6b48efb7afa329d080bd51049e
朱建儒 <864156480@qq.com>
2021-08-22 23:30:05 +0800

    simple persistence framework init

---
+++ myPersistenceClient/src/test/java/com/code/mypersistence/client/MyPersistenceClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.mypersistence.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPersistenceClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
